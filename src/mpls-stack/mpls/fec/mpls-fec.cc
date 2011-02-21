03345f671065321617a3d494eaaa6f4469697f57
aachurin <aachurin@gmail.com>
2011-02-21 19:35:45 +0000

    #

--- src/mpls-stack/mpls/fec/mpls-fec.cc
+++ src/mpls-stack/mpls/fec/mpls-fec.cc
@@ -16,6 +16,7 @@
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  *
  * Author: Andrey Churin <aachurin@gmail.com>
+ *         Stefano Avallone <stavallo@gmail.com>
  */
 
 #include <iomanip>
