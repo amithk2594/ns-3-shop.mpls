13d7fae62a65b91c359eadbbc870d18ad734ecc0
Andrey Churin (aachurin <Andrey Churin (aachurin@gmail.com)>
2011-03-12 18:23:11 +0300

    '#'

--- src/mpls/helpers/mpls-interface-helper.cc
+++ src/mpls/helpers/mpls-interface-helper.cc
@@ -36,8 +36,98 @@
 #include "mpls-node-helper.h"
 
 
+NS_LOG_COMPONENT_DEFINE ("MplsInterfaceHelper");
+
 namespace ns3 {
 
+MplsInterfaceHelper::MplsInterfaceHelper ()
+{
+  m_stream = Create<OutputStreamWrapper> (&std::cout);
+}
 
+MplsInterfaceHelper::MplsInterfaceHelper (const Ptr<OutputStreamWrapper> &stream)
+  : m_stream (stream)
+{
+}
+
+MplsInterfaceHelper::~MplsInterfaceHelper ()
+{
+  m_stream = 0;
+}
+
+template <class T>
+void
+MplsInterfaceHelper::EnableInterfaceAutoInstall (T node) const
+{
+  ForEachNode (node, MakeCallback (&MplsInterfaceHelper::EnableInterfaceAutoInstallInternal, this));
+}
+
+template <class T>
+void
+MplsInterfaceHelper::DisableInterfaceAutoInstall (T node) const
+{
+  ForEachNode (node, MakeCallback (&MplsInterfaceHelper::DisableInterfaceAutoInstallInternal, this));
+}
+
+template <class T>
+void
+MplsInterfaceHelper::PrintInterfaces (T node) const
+{
+  ForEachNode (node, MakeCallback (&MplsInterfaceHelper::PrintInterfacesInternal, this));
+}
+
+void
+MplsInterfaceHelper::SetOutputStream (const Ptr<OutputStreamWrapper> &stream)
+{
+  NS_ASSERT_MSG (stream != 0, "MplsInterfaceHelper::SetOutputStream (): invalid stream");
+  m_stream = stream;
+}
+
+void
+MplsInterfaceHelper::EnableInterfaceAutoInstallInternal (Ptr<Node> node) const
+{
+  Ptr<mpls::MplsProtocol> mpls = node->GetObject<mpls::MplsProtocol> ();
+  NS_ASSERT_MSG (mpls != 0, "MplsInterfaceHelper::DisableInterfaceAutoInstall (): Install MPLS first");
+  mpls->EnableInterfaceAutoInstall ();
+}
+
+void
+MplsInterfaceHelper::DisableInterfaceAutoInstallInternal (Ptr<Node> node) const
+{
+  Ptr<mpls::MplsProtocol> mpls = node->GetObject<mpls::MplsProtocol> ();
+  NS_ASSERT_MSG (mpls != 0, "MplsInterfaceHelper::DisableInterfaceAutoInstall (): Install MPLS first");
+  mpls->DisableInterfaceAutoInstall ();
+}
+
+void
+MplsInterfaceHelper::PrintInterfacesInternal (Ptr<Node> node) const
+{
+  Ptr<mpls::MplsProtocol> mpls = node->GetObject<mpls::MplsProtocol> ();
+  NS_ASSERT_MSG (mpls != 0, "MplsInterfaceHelper::DisableInterfaceAutoInstall (): Install MPLS first");
+
+  //std::ostream *os = m_stream->GetStream ();
+
+  //std::cout << &std::cout << " " << os << std::endl;
+//  os << "Node " << node->GetSystemId () << "-" << node->GetId () << " MPLS interfaces:" << std::endl;
+//  os << std::setiosflags(std::ios::left);
+
+//  for (uint32_t i = 0; i < mpls->GetNInterfaces (); ++i)
+//    {
+//      Ptr<mpls::Interface> iface = mpls->GetInterface (i);
+//      Ptr<NetDevice> dev = iface->GetDevice ();
+//      os << "  if" << std::setw(10) << i << "dev" << std::setw(5) << dev->GetIfIndex ();
+//      PrintDeviceInformation (os, dev);
+//      os << std::endl;
+//    }
+}
+
+void
+MplsInterfaceHelper::PrintDeviceInformation (std::ostream &os, Ptr<NetDevice> device) const
+{
+  Address hwaddr = device->GetAddress ();
+
+  os << "HWaddr "
+     << Mac48Address::IsMatchingType (hwaddr) ? Mac48Address::ConvertFrom (hwaddr) : hwaddr;
+}
 
 } // namespace ns3
